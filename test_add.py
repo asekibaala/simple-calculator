59d857d6f3fcd36d8ffa1bb62c0fbfe71678685a
asekibaala <54054280+asekibaala@users.noreply.github.com>
2023-06-07 07:16:58 +0300

    fixed bug in unit test for add function

--- test_add.py
+++ test_add.py
@@ -2,7 +2,7 @@ import unittest
 from calculator import add
 class _TestAdd(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 6)
+        self.assertEqual(add(2, 3), 5)
 
 if __name__ == '__main__':
     unittest.main()
